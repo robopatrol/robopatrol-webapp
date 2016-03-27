468d9f8a45f1321cb5d342959fc30b56bd21c8f0
Marco Strobel <mail@strobel.ch>
2016-03-27 14:06:20 +0200

    update package metadata

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 export class App {
   configureRouter(config, router) {
-    config.title = 'Aurelia';
+    config.title = 'Robopatrol';
     config.map([
       { route: ['', 'welcome'], name: 'welcome',      moduleId: 'welcome',      nav: true, title: 'Welcome' },
       { route: 'users',         name: 'users',        moduleId: 'users',        nav: true, title: 'Github Users' },
