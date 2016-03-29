e48cdbe879776fb133d31c67a01458ab384df586
Marco Strobel <mail@strobel.ch>
2016-03-29 11:23:27 +0200

    try automatically to connect on app start

--- src/pages/connect.js
+++ src/pages/connect.js
@@ -6,9 +6,13 @@ import {Ros} from 'lib/ros';
 @inject(Ros)
 export class Connect {
   url = 'localhost:9090';
+  autoConnect = true;
 
   constructor(ros, router) {
     this.ros = ros;
+    if (this.autoConnect) {
+      this.submit();
+    }
   }
 
   submit() {
