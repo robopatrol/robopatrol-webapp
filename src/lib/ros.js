ef65ad38fbc0edc64230b02a0645e4069e840612
Marco Strobel <mail@strobel.ch>
2016-03-29 03:04:31 +0200

    add ros connection event listeners

--- src/lib/ros.js
+++ src/lib/ros.js
@@ -1,5 +1,33 @@
+import {inject} from 'aurelia-framework';
+import {EventAggregator} from 'aurelia-event-aggregator';
+import {Router} from 'aurelia-router';
 
-export class Ros extends ROSLIB.Ros {}
+
+@inject(Router, EventAggregator)
+export class Ros extends ROSLIB.Ros {
+
+  constructor(router, ea) {
+    super();
+
+    this.router = router;
+    this.ea = ea;
+
+    this.on('error', function(error) {
+        this.ea.publish('notification', {type: "danger", msg: `Error connecting to websocket server: ${error}`});
+    }.bind(this));
+
+    this.on('connection', function() {
+        this.ea.publish('notification', {type: "success", msg: "Connected to websocket server."});
+        this.router.navigate('');
+    }.bind(this));
+
+    this.on('close', function() {
+        this.ea.publish('notification', {type: "warning", msg: "Connection to websocket server closed."});
+        this.router.navigate('');
+    }.bind(this));
+
+  }
+}
 
 export class Topic extends ROSLIB.Topic {}
 
