d8d6b8250f4a618d857ad044e1e1ea2160339be9
Marco Strobel <mail@strobel.ch>
2016-04-05 14:11:03 +0200

    init express web server application

---
+++ app.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+var debug = require('debug')('robopatrol-webapp');
+
+
+var app = express();
+
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+    next();
+};
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(allowCrossDomain);
+
+app.set('port', process.env.PORT || 9000);
+
+var server = app.listen(app.get('port'), function() {
+  debug('Express server listening on port ' + server.address().port);
+});
+
+
+module.exports = app;
--- build/tasks/serve.js
+++ build/tasks/serve.js
@@ -1,38 +1,48 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync');
+var nodemon = require('gulp-nodemon');
+
+gulp.task('nodemon', function (cb) {
+  var called = false;
+  return nodemon({
+    script: 'app.js'
+  })
+  .on('start', function onStart() {
+    if (!called) {
+      cb();
+    }
+    called = true;
+  })
+  .on('restart', function onRestart() {
+
+    setTimeout(function reload() {
+      browserSync.reload({
+        stream: false
+      });
+    }, 500);
+  });
+});
 
 // this task utilizes the browsersync plugin
 // to create a dev server instance
 // at http://localhost:9000
-gulp.task('serve', ['build'], function(done) {
+gulp.task('serve', ['build', 'nodemon'], function(done) {
   browserSync({
+    proxy: 'http://localhost:9000',
     online: false,
     open: false,
-    port: 9000,
-    server: {
-      baseDir: ['./public'],
-      middleware: function(req, res, next) {
-        res.setHeader('Access-Control-Allow-Origin', '*');
-        next();
-      }
-    }
+    port: 7000
   }, done);
 });
 
 // this task utilizes the browsersync plugin
 // to create a dev server instance
 // at http://localhost:9000
-gulp.task('serve-bundle', ['bundle'], function(done) {
+gulp.task('serve-bundle', ['bundle', 'nodemon'], function(done) {
   browserSync({
+    proxy: 'http://localhost:9000',
     online: false,
     open: false,
-    port: 9000,
-    server: {
-      baseDir: ['./public'],
-      middleware: function(req, res, next) {
-        res.setHeader('Access-Control-Allow-Origin', '*');
-        next();
-      }
-    }
+    port: 7000
   }, done);
 });
