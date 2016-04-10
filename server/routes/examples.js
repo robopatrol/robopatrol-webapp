298a35e8829a6d236e1e890fbf0a256d9675167b
Marco Strobel <mail@strobel.ch>
2016-04-10 11:39:20 +0200

    use persistent storage for example rest api

--- server/routes/examples.js
+++ server/routes/examples.js
@@ -1,69 +1,67 @@
 var express = require('express');
+var models = require('../models');
 var router = express.Router();
 
-// dummy data
-var data = [{id: 37, name: 'example 1'}, {id: 23, name: 'example 2'}];
 
 router.route('/')
   // route for GET /api/examples
   .get(function(req, res) {
-    res.json({data: data});
+    models.Example.findAll()
+      .then(function(data) {
+        res.json({data: data});
+    });
   })
   // route for POST /api/examples
   .post(function(req, res) {
-    var record = req.body;
-    // id is between 1 and 100
-    record.id = Math.floor((Math.random() * 100) + 1);
-    data.push(record);
-    res.json({data: record});
+    models.Example.create(req.body)
+      .then(function(data) {
+        res.json({data: data});
+      });
   });
 
 router.route('/:id')
   // route for GET /api/examples/:id
   .get(function(req, res, next) {
-    var record;
-    var id = req.params.id;
-    data.forEach(d => {
-      if (d['id'] == id) {
-        record = d;
-      }
-    });
-    if (!record) {
-      res.status(404).json({error: "Example '" + id + "' not found"});
-    } else {
-      res.json({data: record});
-    }
+    models.Example.findById(req.params.id)
+      .then(function(data) {
+        if (data === null) {
+          res.status(404).json({error: "Example '" + req.params.id + "' not found"});
+        } else {
+          res.json({data: data});
+        }
+      });
   })
   // route for PUT /api/examples/:id
   .put(function(req, res) {
-    var record;
-    var id = req.params.id;
-    data.forEach(d => {
-      if (d['id'] == id) {
-        record = Object.assign(d, req.body);
+    delete req.body['id'];
+    models.Example.update(req.body, {
+      where: {
+        id: req.params.id
       }
+    }).then(function(numberOfUpdatedRows) {
+      models.Example.findById(req.params.id)
+        .then(function(data) {
+          if (data === null) {
+            res.status(404).json({error: "Example '" + req.params.id + "' not found"});
+          } else {
+            res.json({data: data});
+          }
+        });
     });
-    if (!record) {
-      res.status(404).json({error: "Example '" + id + "' not found"});
-    } else {
-      res.json({data: record});
-    }
   })
   // route for DELETE /api/examples/:id
   .delete(function(req, res) {
-    var ix = -1;
-    var id = req.params.id;
-    data.forEach((el, i, a) => {
-      if (el['id'] == id) {
-        ix = i;
+    models.Example.destroy({
+      where: {
+        id: req.params.id
+      }
+    }).then(function(numberOfDestroyedRows) {
+      if (numberOfDestroyedRows === 0) {
+        res.status(404).json({error: "Example '" + req.params.id+ "' not found"});
+      } else {
+        res.json({message: 'Deleted'});
       }
     });
-    if (ix < 0) {
-      res.status(404).json({error: "Example '" + id + "' not found"});
-    } else {
-      data.splice(ix, 1);
-      res.json({message: 'Deleted'});
-    }
   });
 
 module.exports = router;
