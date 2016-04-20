f6a094bdbe08788f34714f69914a5fb398295919
Marco Strobel <mail@strobel.ch>
2016-04-21 00:26:54 +0200

    add map navigation example

--- src/pages/examples/index.js
+++ src/pages/examples/index.js
@@ -2,8 +2,9 @@
 export class Examples {
   configureRouter(config, router) {
     config.map([
-      { route: '', redirect: 'rest' },
+      { route: '', redirect: 'map-navigation' },
       { route: 'rest', name: 'rest', moduleId: './rest/index', nav: true, title: 'REST API' },
+      { route: 'map-navigation', name: 'map-navigation', moduleId: './map-navigation', nav: true, title: 'Map Navigation' },
       { route: 'dummy-move', name: 'dummy-move', moduleId: './dummy-move', nav: true, title: 'Dummy Move' }
     ]);
 
---
+++ src/pages/examples/map-navigation.js
@@ -0,0 +1,37 @@
+import {inject} from 'aurelia-framework';
+
+import {Ros} from 'lib/ros';
+
+@inject(Ros)
+export class MapNavigation {
+
+  constructor(ros) {
+    this.ros = ros;
+  }
+
+  attached() {
+    // Create the main viewer.
+    var viewer = new ROS2D.Viewer({
+      divID : 'nav',
+      width : 400,
+      height : 400
+    });
+
+    // Setup the nav client.
+    var nav = NAV2D.OccupancyGridClientNav({
+      ros : this.ros,
+      rootObject : viewer.scene,
+      viewer : viewer,
+      serverName : '/move_base'
+    });
+
+    // example map click listener
+    viewer.scene.addEventListener('click', (event) => {
+      var coords = viewer.scene.getStage().globalToRos(event.stageX, event.stageY);
+      var pose = new ROSLIB.Pose({
+        position : new ROSLIB.Vector3(coords)
+      });
+      console.log("Coords:\nx: " + coords.x + "\ny: " + coords.y + "\nz: " + coords.z);
+    });
+  }
+}
