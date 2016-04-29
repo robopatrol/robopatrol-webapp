7057426634c772818077a78ab44e3ff09edae095
Marco Strobel <mail@strobel.ch>
2016-04-29 22:48:43 +0200

    move http client configuration to connect page

--- src/pages/connect.js
+++ src/pages/connect.js
@@ -1,19 +1,21 @@
 import {inject} from 'aurelia-framework';
 import {Router} from 'aurelia-router';
 import {EventAggregator} from 'aurelia-event-aggregator';
+import {HttpClient} from 'aurelia-fetch-client';
 
 import {Ros} from 'lib/ros';
 
 
-@inject(Ros, Router, EventAggregator)
+@inject(Ros, Router, HttpClient, EventAggregator)
 export class Connect {
   url = 'localhost:9090';
   autoConnect = true;
 
-  constructor(ros, router, ea) {
+  constructor(ros, router, http, ea) {
     this.ros = ros;
     this.router = router;
-    this.ea = ea
+    this.http = http;
+    this.ea = ea;
   }
 
   activate(params, routeConfig, navigationInstruction){
@@ -26,6 +28,29 @@ export class Connect {
   submit() {
     //TODO: url validation (?)
     return this.ros.connect(`ws:\/\/${this.url}`).then(() => {
+      // TODO: http configuration
+      /*http.configure(config => {
+        config
+          .withBaseUrl(`http:\/\/${this.url}`)
+          .withDefaults({
+            headers: {
+              'Accept': 'application/json'
+            }
+          })
+          .withInterceptor({
+            request(request) {
+              return request;
+            },
+            response(response) {
+              // TODO: take care of other 2XX success codes
+              if (response.status !== 200) {
+              	throw response;
+              } else {
+              	return response;
+              }
+            }
+        });
+      });*/
       this.ea.publish('notification', {type: "success", msg: "Connected to websocket server."});
       this.router.navigate(this.nextRoute);
     }).catch((error) => {
