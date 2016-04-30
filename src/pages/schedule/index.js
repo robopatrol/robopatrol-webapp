796e5047f344e6ad142d7865f11442157832b3bd
Stefanie Wehrle <steffinchen@gmail.com>
2016-04-30 12:25:00 +0200

    methods to edit / delete entries ( not yet complete)

--- src/pages/schedule/index.js
+++ src/pages/schedule/index.js
@@ -5,22 +5,30 @@ import {EditSchedule} from './editSchedule';
 @inject(DialogService)
 export class Schedule {
 
-  //dummy entries
-  entries = [{
-    id: 1,
-    name: "Hourly Patrol",
-    description: "This should keep the cats at bay!",
-    cron: "0 * * * * "
-  }, {
-    id: 2,
-    name: "Daily Patrol",
-    description: "Make sure the dog doesn't do anything stupid",
-    cron: "0 15 * * *"
-  }];
+  entries = [];
 
   constructor(dialogService) {
     this.dialogService = dialogService;
+  }
+
+  activate() {
+    this.loadSchedules();
+  }
+
+  loadSchedules() {
     // TODO load exiting schedules from server and add to entries
+    //dummy entries
+    this.entries = [{
+      id: 1,
+      name: "Hourly Patrol",
+      description: "This should keep the cats at bay!",
+      cron: "0 * * * * "
+    }, {
+      id: 2,
+      name: "Daily Patrol",
+      description: "Make sure the dog doesn't do anything stupid",
+      cron: "0 15 * * *"
+    }];
   }
 
   addSchedule() {
@@ -38,5 +46,30 @@ export class Schedule {
         });
       }
     });
+  }
+
+  editSchedule(entry) {
+    console.log("editing: ");
+    console.log(entry);
+
+    this.dialogService.open({
+      viewModel: EditSchedule,
+      model: entry
+    }).then(response => {
+      if (!response.wasCancelled) {
+        console.log('good - ', response.output);
+        //TODO  save changes to REST server
+        //TODO if save not successful, refresh entries list
+      }
+    });
+
+  }
+
+  deleteSchedule(entry) {
+    console.log("deleting:");
+    console.log(entry);
+
+    //TODO confirm deletion
+    //TODO if confirmed, send delete to REST server, if not successful  refresh entries list
   }
 }
