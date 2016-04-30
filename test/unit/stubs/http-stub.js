4ba990b4e01d35d07e0c0a54a77d1fc531e91eae
Marco Strobel <mail@strobel.ch>
2016-04-30 12:29:30 +0200

    fix http fetch client configuration

---
+++ test/unit/stubs/http-stub.js
@@ -0,0 +1,12 @@
+export class HttpStub {
+  fetch(url) {
+    var response = this.itemStub;
+    this.url = url;
+    return new Promise((resolve) => {
+      resolve({ json: () => response });
+    });
+  }
+
+  configure(func) {
+  }
+}
