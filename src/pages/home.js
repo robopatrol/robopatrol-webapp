41b1eaacc0b73383683d284b1819618aa0712792
Joni <joni.werren@gmail.com>
2016-05-02 11:03:34 +0200

    Initial startpage
    first look and feel for the startpage

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,4 +1,10 @@
 
 export class Home {
   heading = 'Welcome to Robopatrol';
+  route = 'Waypoints';
+  config = 'Settings';
+  charging = 'Battery status';
+  error = 'Warnings/Errors';
+  images = 'Images';
+  home = 'Home';
 }
