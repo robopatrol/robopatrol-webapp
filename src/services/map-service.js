168bae6b913c6e1b56162d18d1574c9df394c14c
Marco Strobel <mail@strobel.ch>
2016-05-05 18:34:10 +0200

    add map-service to fetch and cache occupancy-grid

---
+++ src/services/map-service.js
@@ -0,0 +1,33 @@
+import {inject} from 'aurelia-framework';
+import {Ros, Service, ServiceRequest} from '../lib/ros';
+
+@inject(Ros)
+export class MapService {
+
+  cachedOccupancyGrid = null;
+
+  constructor(ros) {
+    this.ros = ros;
+
+    this.staticMapService= new Service({
+      ros: this.ros,
+      name : '/static_map',
+      serviceType : 'nav_msgs/GetMap'
+    });
+
+    this.request = new ServiceRequest();
+  }
+
+  getMap(force) {
+    return new Promise((resolve, reject) => {
+      if (this.cachedOccupancyGrid && !force) {
+        return resolve(this.cachedOccupancyGrid);
+      } else {
+        this.staticMapService.callService(this.request, (response) => {
+          this.cachedOccupancyGrid = response.map;
+          resolve(this.cachedOccupancyGrid);
+        });
+      }
+    });
+  }
+}
