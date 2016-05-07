8f6e3e73bdfa04100609278f09fbf7c8caace002
Marco Strobel <mail@strobel.ch>
2016-05-07 13:31:36 +0200

    add map service tests

--- test/unit/map-service.spec.js
+++ test/unit/map-service.spec.js
@@ -1,5 +1,5 @@
 import {MapService, MapImage} from '../../src/services/map-service';
-import {RosStub} from './stubs/ros-stub';
+import {RosStub, ServiceStub} from './stubs/ros-stub';
 
 
 describe('the map image', () => {
@@ -44,10 +44,82 @@ describe('the map image', () => {
 
 describe('the map service module', () => {
   var sut;
-  var mokedRos;
+  var mockedRos;
+  var mockedService;
 
   beforeEach(() => {
-    mokedRos = new RosStub();
+    mockedRos = new RosStub();
+    sut = new MapService(mockedRos);
+  });
+
+  it('contains a ros property', () => {
+    expect(sut.ros).toBeDefined();
+  });
+
+  it('configures the ros property', () => {
+    expect(sut.ros).toEqual(mockedRos);
+  });
+
+  it('contains a static map property', () => {
+    expect(sut.staticMapImage).toBeDefined();
+  });
+
+  it('contains a static map service property', () => {
+    expect(sut.staticMapService).toBeDefined();
+  });
+
+  it('contains a static map service request property', () => {
+    expect(sut.staticMapServiceRequest).toBeDefined();
+  });
+});
+
+describe('the map service get static map function', () => {
+  var sut;
+  var mockedRos;
+  var mockedService;
+
+  beforeEach(() => {
+    mockedRos = new RosStub();
+    mockedService = new ServiceStub();
+    sut = new MapService(mockedRos);
+    sut.staticMapService = mockedService;
+  });
+
+  it('calls the ros service if no image is loaded', () => {
+    spyOn(mockedService, 'callService');
+    sut.getStaticMapImage();
+    expect(mockedService.callService).toHaveBeenCalled();
+  });
+
+  it('skips the ros service call if image is already loaded', () => {
+    sut.staticMapImage = 'is not null or undefined';
+    spyOn(mockedService, 'callService');
+    sut.getStaticMapImage();
+    expect(mockedService.callService).not.toHaveBeenCalled();
+  });
+
+  it('calls the ros service if enforeced', () => {
+    sut.staticMapImage = 'is not null or undefined';
+    spyOn(mockedService, 'callService');
+    sut.getStaticMapImage(true);
+    expect(mockedService.callService).toHaveBeenCalled();
+  });
+
+});
+
+
+describe('the map service grid convert function', () => {
+  var sut;
+  var msgInfo = { width: 4, height: 2, resolution: 0.5 };
+  var msgData = [100, 100, 100, 0, -1, -1, 0, 0];
+  var image;
+
+  beforeEach(() => {
     sut = new MapService();
+    image = sut.convertOccupancyGridToMapImage({ info: msgInfo, data: msgData });
+  });
+
+  it('converts occupancy grid msg to base64 data', () => {
+    expect(image.data.startsWith('data:image/png;base64'));
   });
 });
