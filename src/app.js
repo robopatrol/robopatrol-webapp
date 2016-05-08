2be64c49826e8876602960f479113b3f19e52167
Joni <joni.werren@gmail.com>
2016-05-08 23:07:11 +0200

    Update startpage
    added links and aurelia template, separation for last patrol

--- src/app.js
+++ src/app.js
@@ -11,15 +11,27 @@ export class App {
     config.title = 'Robopatrol';
     config.addPipelineStep('preActivate', ConnectionStep)
     config.map([
-      { route: ['', 'home'], name: 'home', moduleId: 'pages/home', nav: true, title: 'Home', connected: true },
-      { route: 'connect', name: 'connect', moduleId: 'pages/connect', nav: false, title: 'Connect', connected: false },
+      { route: ['', 'home'], name: 'home', moduleId: 'pages/home', nav: true, title: 'Home', connected: true, settings: { startpage: false, last: false, icon: '/img/page/home.png' } },
 
-      { route: 'teleop', name: 'teleop', moduleId: 'pages/teleop/index', nav: true, title: 'Teleop', connected: true },
-      { route: 'ros-infos', name: 'ros-infos', moduleId: 'pages/ros-infos/index', nav: true, title: 'ROS Infos', connected: true },
+      { route: 'connect', name: 'connect', moduleId: 'pages/connect', nav: false, title: 'Connect', connected: false, settings: { startpage: false, last: false, icon: '' } },
 
-      { route: 'examples', name: 'examples', moduleId: 'pages/examples/index', nav: true, title: 'Examples', connected: true },
+      { route: 'teleop', name: 'teleop', moduleId: 'pages/teleop/index', nav: true, title: 'Teleop', connected: true, settings: { startpage: false, last: false, icon: '' } },
 
-      { route: 'disconnect', name: 'disconnect', moduleId: 'pages/disconnect', nav: true, title: 'Disconnect', connected: true },
+      { route: 'ros-infos', name: 'ros-infos', moduleId: 'pages/ros-infos/index', nav: true, title: 'ROS Infos', connected: true, settings: { startpage: false, last: false, icon: '' } },
+
+      { route: 'examples', name: 'examples', moduleId: 'pages/examples/index', nav: true, title: 'Examples', connected: true, settings: { startpage: false, last: false, icon: '' } },
+
+      { route: 'disconnect', name: 'disconnect', moduleId: 'pages/disconnect', nav: true, title: 'Disconnect', connected: true, settings: { startpage: false, last: false, icon: '' } },
+
+      { route: 'scheduler', name: 'scheduler', moduleId: 'pages/scheduler', nav: true, title: 'Schedule route', connected: true, settings: { startpage: true, last: false, icon: '/img/page/route.png' } },
+
+      { route: 'battery', name: 'battery', moduleId: 'pages/ros-infos/battery', nav: true, title: 'Battery status', connected: true, settings: { startpage: true, last: false, icon: '/img/page/battery.png' } },
+
+      { route: 'images', name: 'images', moduleId: 'pages/images', nav: true, title: 'Images', connected: true, settings: { startpage: true, last: true, icon: '/img/page/picture.png' } },
+
+      { route: 'warnings', name: 'warnings', moduleId: 'pages/warnings', nav: true, title: 'Status', connected: true, settings: { startpage: true, last: true, icon: '/img/page/warning.png' } },
+
+      { route: 'settings', name: 'settings', moduleId: 'pages/settings', nav: true, title: 'Settings', connected: true, settings: { startpage: true, last: false, icon: '/img/page/settings.png' } }
     ]);
 
     this.router = router;
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,10 +1,5 @@
 
 export class Home {
   heading = 'Welcome to Robopatrol';
-  route = 'Waypoints';
-  config = 'Settings';
-  charging = 'Battery status';
-  error = 'Warnings/Errors';
-  images = 'Images';
-  home = 'Home';
+  patrol = 'Last patrol';
 }
