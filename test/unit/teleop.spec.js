1b3998654895a617bfa48110d8ae8f2409d88627
Stefanie Wehrle <steffinchen@gmail.com>
2016-05-21 14:11:35 +0200

    lodash as dependency, more tests

--- test/unit/teleop.spec.js
+++ test/unit/teleop.spec.js
@@ -1,9 +1,135 @@
 import {Teleop} from '../../src/pages/teleop/index';
+import {_} from 'lodash';
 
 describe('the teleop module', () => {
   var sut;
 
   beforeEach(() => {
     sut = new Teleop();
+  });
+
+  it('should define an action for each button', ()=>{
+    expect(sut.buttons).toBeDefined();
+    _.forEach(sut.buttons, (row)=> {
+      _.forEach(row, (btn) => {
+        expect(btn.action).toBeDefined();
+        expect(btn.icon).toBeDefined();
+      });
+    });
+  });
+
+  it('moves forward', () => {
+    spyOn(sut.cmdVel, 'publish');
+    sut.speed = 5;
+
+    sut.executeAction('forwards');
+
+    expect(sut.twist.linear.x).toBe(5);
+    expect(sut.twist.angular.z).toBe(0);
+    expect(sut.cmdVel.publish).toHaveBeenCalled();
+  });
+
+  it('moves forward-left', () => {
+    spyOn(sut.cmdVel, 'publish');
+    sut.speed = 5;
+    sut.turn = 8;
+
+    sut.executeAction('forwards-left');
+
+    expect(sut.twist.linear.x).toBe(5);
+    expect(sut.twist.angular.z).toBe(8);
+    expect(sut.cmdVel.publish).toHaveBeenCalled();
+  });
+
+  it('moves forward right', () => {
+    spyOn(sut.cmdVel, 'publish');
+    sut.speed = 5;
+    sut.turn = 8;
+
+    sut.executeAction('forwards-right');
+
+    expect(sut.twist.linear.x).toBe(5);
+    expect(sut.twist.angular.z).toBe(-8);
+    expect(sut.cmdVel.publish).toHaveBeenCalled();
+  });
+
+  it('moves left', () => {
+    spyOn(sut.cmdVel, 'publish');
+    sut.turn = 5;
+
+    sut.executeAction('left');
+
+    expect(sut.twist.linear.x).toBe(0);
+    expect(sut.twist.angular.z).toBe(5);
+    expect(sut.cmdVel.publish).toHaveBeenCalled();
+  });
+
+  it('moves right', () => {
+    spyOn(sut.cmdVel, 'publish');
+    sut.turn = 5;
+
+    sut.executeAction('right');
+
+    expect(sut.twist.linear.x).toBe(0);
+    expect(sut.twist.angular.z).toBe(-5);
+    expect(sut.cmdVel.publish).toHaveBeenCalled();
+  });
+
+  it('stops', () => {
+    spyOn(sut.cmdVel, 'publish');
+    sut.speed = 5;
+
+    sut.executeAction('stop');
+
+    expect(sut.twist.linear.x).toBe(0);
+    expect(sut.twist.angular.z).toBe(0);
+    expect(sut.cmdVel.publish).toHaveBeenCalled();
+  });
+
+  it('moves backward', () => {
+    spyOn(sut.cmdVel, 'publish');
+    sut.speed = 5;
+
+    sut.executeAction('backwards');
+
+    expect(sut.twist.linear.x).toBe(-5);
+    expect(sut.twist.angular.z).toBe(0);
+    expect(sut.cmdVel.publish).toHaveBeenCalled();
+  });
+
+  it('moves backwards-left', () => {
+    spyOn(sut.cmdVel, 'publish');
+    sut.speed = 5;
+    sut.turn = 8;
+
+    sut.executeAction('backwards-left');
+
+    expect(sut.twist.linear.x).toBe(-5);
+    expect(sut.twist.angular.z).toBe(-8);
+    expect(sut.cmdVel.publish).toHaveBeenCalled();
+  });
+
+  it('moves backwards-right', () => {
+    spyOn(sut.cmdVel, 'publish');
+    sut.speed = 5;
+    sut.turn = 8;
+
+    sut.executeAction('backwards-right');
+
+    expect(sut.twist.linear.x).toBe(-5);
+    expect(sut.twist.angular.z).toBe(8);
+    expect(sut.cmdVel.publish).toHaveBeenCalled();
+  });
+
+  it('doesnt move by default', () => {
+    spyOn(sut.cmdVel, 'publish');
+    sut.speed = 5;
+    sut.turn = 8;
+
+    sut.executeAction('foo');
+
+    expect(sut.twist.linear.x).toBe(0);
+    expect(sut.twist.angular.z).toBe(0);
+    expect(sut.cmdVel.publish).toHaveBeenCalled();
   });
 });
